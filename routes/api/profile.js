d7f2d360a1b593f8aa74a5f840cb89ee4e70c0cb
NasiaKayVuong <nasia.kayvuong@gmail.com>
2021-08-14 15:41:47 -0700

    Created posts Api and routes.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
-const Profile = require('../../models/Profile')
-const User = require('../../models/User')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route GET api/profile/me
 // @desc Get current users profile
