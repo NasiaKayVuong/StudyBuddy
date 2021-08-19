b03c69609c5d3834cdad0dccbf0d10cfbaaf5a9b
NasiaKayVuong <nasia.kayvuong@gmail.com>
2021-08-19 12:45:36 -0700

    Set up Redux store and created alerts.

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 export const Landing = () => {
-    return (
-        <section className="landing">
-            <div className="dark-overlay">
-            <div className="landing-inner">
-                <h1 className="x-large">Find a Study Partner</h1>
-                <p className="lead">
-                Create a profile/portfolio, share posts, get help, and connect with other like-minded students
-                </p>
-                <div className="buttons">
-                <Link to="/register" className="btn btn-primary">Sign Up</Link>
-                <Link to="/login" className="btn btn-light">Login</Link>
-                </div>
-            </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Find a Study Partner</h1>
+          <p className='lead'>
+            Create a profile/portfolio, share posts, get help, and connect with
+            other like-minded students
+          </p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>
+              Sign Up
+            </Link>
+            <Link to='/login' className='btn btn-light'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
